f65a5187446f55872308255b1dd5c77c3b3d73f6
Enanodr <enanodr@gmail.com>
2015-06-09 17:59:01 -0300

    se viene el comiiiiit locooooooooooo

--- ping_emitter.py
+++ ping_emitter.py
@@ -9,16 +9,19 @@ import time
 class TraceRoute:
     def __init__(self, ip_dst):
         self.ip_dst = ip_dst
+        total_avg_rtt = 0
+        total_std_rtt = 0
         
     def trace_and_process(self, time_to_live, times_repeat):
         steps = self.trace(time_to_live, times_repeat)
         self.process(steps)
         for step in steps:
-            print step
+            step.print(self.total_avg_rtt, self.total_std_rtt)
 
     def process(self, steps):
         all_rtts = [ step.rtt() for step in steps if step.rtt() != 'Unkwown']
-        total_avg_rtt = sum(all_rtts) / float(len(all_rtts)) 
+        self.total_avg_rtt = sum(all_rtts) / float(len(all_rtts)) 
+        self.total_std_rtt = numpy.std(all_rtts)
         #complete steps with route_rtt
         for step in steps:
             step.avg_route_rtt(total_avg_rtt)
@@ -26,6 +29,7 @@ class TraceRoute:
     def trace(self, time_to_live, times_repeat):
         responses = []
         general_counter = 1
+        acumulated_time = 0
         for i in range(1, time_to_live + 1):
             print 'Tracing with TTL: ' + str(i)
             step = Step(i)
@@ -37,13 +41,13 @@ class TraceRoute:
                 time_before = time.clock()
                 answered, unanswered = sr(packet, timeout=1, verbose=False)
                 time_after = time.clock()
-                ellapsed = time_after - time_before
-
+                ellapsed = time_after - time_before - acumulated_time
                 if len(answered) == 1:
                     ip = answered[ICMP][0][1].src #arreglar estos magics numbers
                     step.append(ip, ellapsed)
 
             responses.append(step)
+            acumulated_time += step.avg()
         return responses
 
    
@@ -51,16 +55,12 @@ class Step:
     def __init__(self, ttl):
         self.ttl = ttl
         self.ip_ellapsed = dict()
-        self.total_avg_rtt = 0
 
     def append(self, ip, ellapsed):
         if self.ip_ellapsed.has_key(ip):
             self.ip_ellapsed[ip].append(ellapsed)
         else:
             self.ip_ellapsed[ip] = [ellapsed]
-
-    def avg_route_rtt(self, total_avg_rtt):
-        self.total_avg_rtt = total_avg_rtt
 
     def rtt(self): #es el RTT entre su nodo anterior y el, NO es el RTT desde el comienzo!
         if self._is_uknown_ip():
@@ -74,17 +74,17 @@ class Step:
             return 'Unkwown'
         return numpy.std(self._ellapsed())
 
-    def zrtt(self):
+    def zrtt(self,total_avg_rtt, total_std_rtt):
         if self._is_uknown_ip():
             return 'Unkwown'            
-        return (self.rtt() - self.total_avg_rtt) / self.std()
+        return (self.rtt() - total_avg_rtt) / total_std_rtt
 
-    def __str__(self):
+    def print(self,total_avg_rtt,total_std_rtt):
         ttl = str(self.ttl)
         rtt = str(self.rtt())
-        zrtt = str(self.zrtt())
+        zrtt = str(self.zrtt(total_avg_rtt,total_std_rtt))
         ips = self.ip_ellapsed.keys()
-        return "TTL: {0}\tAVG: {1}\tZRTT: {2}\tIPs: {3}".format(ttl, rtt, zrtt, ips)
+        print "TTL: {0}\tAVG: {1}\tZRTT: {2}\tIPs: {3}".format(ttl, rtt, zrtt, ips)
 
     def _ellapsed(self):
         ellapsed = []
@@ -104,3 +104,4 @@ else:
     times_repeat = int(sys.argv[3])
     trace_route = TraceRoute(ip_dst)
     trace_route.trace_and_process(time_to_live, times_repeat)
+
