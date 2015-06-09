458a81a627263b69442fac0fe4cd7fb603c8a44f
mtqp <marian_sabianaa@hotmail.com>
2015-06-09 17:20:01 -0300

    refactor + formulas

--- ping_emitter.py
+++ ping_emitter.py
@@ -1,34 +1,106 @@
 import sys
 from datetime import datetime, timedelta
 from scapy.all import *
+import numpy
 import time
 
 #print packet.summary()
 
-def trace_route(ip_dst, time_to_live, times_repeat):
-    for i in range(0, time_to_live):
-        ans_ellapsed = []
-        ip = '*'
-        for j in range(0, times_repeat):
-            packet = IP(dst=ip_dst, ttl=i)/ ICMP()
+class TraceRoute:
+    def __init__(self, ip_dst):
+        self.ip_dst = ip_dst
+        
+    def trace_and_process(self, time_to_live, times_repeat):
+        steps = self.trace(time_to_live, times_repeat)
+        self.process(steps)
+        for step in steps:
+            print step
 
-            time_before = time.clock()
-            answered, unanswered = sr(packet, timeout=1, verbose=False)
-            time_after = time.clock()
-            ellapsed = time_after - time_before
+    def process(self, steps):
+        all_rtts = [ step.rtt() for step in steps if step.rtt() != 'Unkwown']
+        total_avg_rtt = sum(all_rtts) / float(len(all_rtts)) 
+        #complete steps with route_rtt
+        for step in steps:
+            step.avg_route_rtt(total_avg_rtt)
 
-            if len(answered) == 1:
-                ip = answered[ICMP][0][1].src #arreglar estos magics numbers
-                ans_ellapsed.append(ellapsed)
+    def trace(self, time_to_live, times_repeat):
+        responses = []
+        general_counter = 1
+        for i in range(1, time_to_live + 1):
+            print 'Tracing with TTL: ' + str(i)
+            step = Step(i)
+            ip = '*'
 
-        if len(ans_ellapsed) > 0:
-            avg = sum(ans_ellapsed) / len(ans_ellapsed)
-            print str(i) + ' Answered. ' + ip + ' ' + str(avg)
+            for j in range(0, times_repeat):
+                packet = IP(dst=self.ip_dst, ttl=i)/ ICMP(seq=general_counter)
+                general_counter += 1
+                time_before = time.clock()
+                answered, unanswered = sr(packet, timeout=1, verbose=False)
+                time_after = time.clock()
+                ellapsed = time_after - time_before
+
+                if len(answered) == 1:
+                    ip = answered[ICMP][0][1].src #arreglar estos magics numbers
+                    step.append(ip, ellapsed)
+
+            responses.append(step)
+        return responses
+
+   
+class Step:
+    def __init__(self, ttl):
+        self.ttl = ttl
+        self.ip_ellapsed = dict()
+        self.total_avg_rtt = 0
+
+    def append(self, ip, ellapsed):
+        if self.ip_ellapsed.has_key(ip):
+            self.ip_ellapsed[ip].append(ellapsed)
         else:
-            print str(i) + ' Unanswered. ' + ip
+            self.ip_ellapsed[ip] = [ellapsed]
 
+    def avg_route_rtt(self, total_avg_rtt):
+        self.total_avg_rtt = total_avg_rtt
 
-ip_dst = (sys.argv[1])
-time_to_live = int(sys.argv[2])
-times_repeat = int(sys.argv[3])
-trace_route(ip_dst, time_to_live, times_repeat)
+    def rtt(self): #es el RTT entre su nodo anterior y el, NO es el RTT desde el comienzo!
+        if self._is_uknown_ip():
+            return 'Unkwown'
+
+        ellapsed = self._ellapsed()
+        return float(sum(ellapsed)) / len(ellapsed)
+
+    def std(self):
+        if self._is_uknown_ip():
+            return 'Unkwown'
+        return numpy.std(self._ellapsed())
+
+    def zrtt(self):
+        if self._is_uknown_ip():
+            return 'Unkwown'            
+        return (self.rtt() - self.total_avg_rtt) / self.std()
+
+    def __str__(self):
+        ttl = str(self.ttl)
+        rtt = str(self.rtt())
+        zrtt = str(self.zrtt())
+        ips = self.ip_ellapsed.keys()
+        return "TTL: {0}\tAVG: {1}\tZRTT: {2}\tIPs: {3}".format(ttl, rtt, zrtt, ips)
+
+    def _ellapsed(self):
+        ellapsed = []
+        for key in self.ip_ellapsed.keys():
+            if key != '*':
+                ellapsed = ellapsed + self.ip_ellapsed[key]
+        return ellapsed
+    def _is_uknown_ip(self):
+        ellapsed = self._ellapsed()
+        return len(ellapsed) == 0
+
+if (len(sys.argv) < 4):
+    print 'Usage: python ping_emiter.py [ip] [time to live] [times repeat]'
+else:
+    ip_dst = (sys.argv[1])
+    time_to_live = int(sys.argv[2])
+    times_repeat = int(sys.argv[3])
+    trace_route = TraceRoute(ip_dst)
+    trace_route.trace_and_process(time_to_live, times_repeat)
