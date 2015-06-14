7767d7dd86c8d0f67f2ffbb13a421ae2cb52f473
Enanodr <enanodr@gmail.com>
2015-06-14 19:37:24 -0300

    nuevas pruebas

---
+++ raw_data/pings/alpha_n_ping.sh
@@ -0,0 +1,6 @@
+#python ping.py www.auckland.ac.nz 0.1 20 1	>> alpha01_count20_auckland.csv
+#python ping.py www.cam.ac.uk 0.1 20 1 >> alpha01_count20_cambridge.csv
+#python ping.py 162.105.252.133 0.1 20 3 >> alpha01_count20_ultima_que_responde_peking.csv #ultima que responde en peking www.pku.edu.cn
+python ping.py www.ru.ac.za 0.1 20 1 >> alpha01_count20_rhodes.csv
+python ping.py www.sfu.ca 0.1 20 1 >> alpha01_count20_sfu.csv
+
