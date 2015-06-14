4af502758ee47ada95f701b488a786f9281556cf
Enanodr <enanodr@gmail.com>
2015-06-14 16:09:57 -0300

    ping modificado mas datos

---
+++ src/alpha_n_ping.sh
@@ -0,0 +1,5 @@
+python ping.py www.auckland.ac.nz 0.01 20 1	>> 01alpha_20n_auckland.out
+python ping.py www.cam.ac.uk 0.01 20 1 >> 01alpha_20n_cambridge.out
+python ping.py www.pku.edu.cn 0.01 20 3 >> 01alpha_20n_peking.out
+python ping.py www.ru.ac.za 0.01 20 1 >> 01alpha_20n_rhodes.out
+
--- src/ping.py
+++ src/ping.py
@@ -7,46 +7,68 @@ import numpy
 import time
 
 class Ping:
-    def __init__(self, ip, alpha, timeout): 
+    def __init__(self, ip, alpha_pace, count_pace, timeout): 
         self.ip = ip
-        self.alpha = alpha
         self.icmp_id = random.randint(0,65535)
         self.timeout = timeout
+        self.statistics = self.create_statistics(alpha_pace, count_pace)
 
-    def times(self, an_amount):
-        statistics = Statistics(self.ip, self.alpha)
-
-        for i in range(1, an_amount+1):
+    def do(self):
+        for i in range(1, 151):
             packet = IP(dst=self.ip) / ICMP(id=self.icmp_id, seq=i)
             response = sr1(packet, timeout = self.timeout, verbose=False)
 
-            #print 'Ping: ' + str(i)
-
             if response is None:
-                statistics.ping_failed()
+                self.ping_failed(i)                
             else:
                 ellapsed = response.time - packet.time
-                statistics.ping_succeeded(ellapsed)
+                self.ping_succeeded(ellapsed, i)
 
             self.icmp_id = (self.icmp_id + 1) % 65535
     
+        return self.statistics
+
+    def ping_failed(self, i):
+        for statistic in self.statistics:
+            statistic.ping_failed(i)
+
+    def ping_succeeded(self, ellapsed, i):
+        for statistic in self.statistics:
+            statistic.ping_succeeded(ellapsed, i)
+
+    def create_statistics(self, alpha_pace, count_pace):
+        statistics = []
+        alphas = self.alphas(alpha_pace)
+        counts = self.counts(count_pace)
+        for alpha in alphas:
+            for count in counts:
+                statistics.append(Statistic(self.ip, alpha, count))
         return statistics
 
-class Statistics:
-    def __init__(self, ip, alpha):
+    def alphas(self, alpha_pace):
+        return numbers(0.8, 1.0, alpha_pace)
+
+    def counts(self, count_pace):
+        return numbers(10,150, count_pace)
+
+class Statistic:
+    def __init__(self, ip, alpha, max_count):
         self.ip = ip
         self.alpha = alpha
+        self.max_count = max_count
         self.failed_count = 0
         self.succeeded_count = 0
         self.ertt = -1
         self.two_thirds = 2.0/3.0
 
-    def ping_failed(self):
-        self.failed_count += 1
+    def ping_failed(self, i):
+        if i < self.max_count:
+            self.failed_count += 1
 
-    def ping_succeeded(self, ellapsed):
-        self.succeeded_count += 1
-        self.update_ertt(ellapsed)
+    def ping_succeeded(self, ellapsed, i):
+        if i < self.max_count:
+            self.succeeded_count += 1
+            self.update_ertt(ellapsed)
 
     def update_ertt(self, ellapsed):
         if self.succeeded_count > 1:
@@ -61,24 +83,34 @@ class Statistics:
         return self.succeeded_count + self.failed_count
 
     def throughtput(self):
-        den = self.ertt * math.sqrt(self.two_thirds * self.loss_probability()) 
+        den = self.ertt * math.sqrt(self.two_thirds * self.loss_probability())
+        if den == 0.0:
+            return 'No se han perdido paquetes, no se puede calcular el throughput'
+        if self.loss_probability() == 1.0:
+            return 'No se puede estimar el RTT ya que se han perdido todos los paquetes'
         return 1.0/den
 
     def output(self):
-        print 'ip: ' + str(self.ip)
-        print '\talpha: ' + str(self.alpha) + '\ttotal: '  + str(self.total())
-        print '\tfailed: ' + str(self.failed_count) + '\tsucceeded: ' + str(self.succeeded_count) + '\tlossProbability ' + str(self.loss_probability())
-        print '\testimated rtt: ' + str(self.ertt)
-        print '\tthroughput: ' + str(self.throughtput())
+        print str(self.ip) + ',' + str(self.alpha) + ',' + str(self.total()) + ',' + str(self.failed_count) + ',' + str(self.succeeded_count)  + ',' + str(self.loss_probability()) + ',' + str(self.ertt) + ',' + str(self.throughtput())
+
+def numbers(left, right, pace):
+    numbers = []
+    number = left
+    while number <= right:
+        numbers.append(number)
+        number += pace
+    return numbers
 
 if (len(sys.argv) < 4):
-    print 'Usage: python ping.py [ip] [alpha] [count] [timeout]'
+    print 'Usage: python ping.py [ip] [alpha_pace] [count_pace] [timeout]'
 else:
     ip_dst = (sys.argv[1])
-    alpha = float(sys.argv[2])
-    count = int(sys.argv[3])
+    alpha_pace = float(sys.argv[2])
+    count_pace = int(sys.argv[3])
     timeout = float(sys.argv[4])
-    ping = Ping(ip_dst, alpha, timeout)
-    statistics = ping.times(count)
-    statistics.output()
+    ping = Ping(ip_dst, alpha_pace, count_pace, timeout)
+    statistics = ping.do()
+    print 'ip,alpha,total,failed,succeeded,lossProbability,ertt,throughput'
+    for statistic in statistics:
+        statistic.output()
 
